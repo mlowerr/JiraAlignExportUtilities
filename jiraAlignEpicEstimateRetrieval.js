a918bc958537b10a3baccded8dfc96f30f1c546c
mlowerr <35197538+mlowerr@users.noreply.github.com>
2023-08-17 11:25:16 -0500

    Update jiraAlignEpicEstimateRetrieval.js

--- jiraAlignEpicEstimateRetrieval.js
+++ jiraAlignEpicEstimateRetrieval.js
@@ -1,5 +1,8 @@
 // jiraAlignEpicEstimateRetrieval.js
 //
+// Author: Matthew Lowerr
+//
+// Description:
 // This script will extract all team level estimates from the Estimate tab of a Jira Align epic into a downloadable CSV file to be further processed in Excel.
 //
 // Usage:
@@ -7,20 +10,39 @@
 // 2. Open the browsers Dev Tools (in Chrome: More Tools > Developer Tools)
 // 3. Paste the entire contents of this file into the Console tab
 // 4. Import the output file into Excel
+// The lists of Jira Align epics, program increments, investable structures and programs to extract data for
 
-// The lists of Jira Align epics, program increments, and programs to extract data for
-let epicList = [10266, 10265];
-let piList = [291, 292, 293, 294];
+// Update with all epics in scope for data retrieval
+let epicList = [12213, 10604, 10660, 11900];
+
+let piList = [{
+    Name: "PI-17A",
+    ID: 291
+}, {
+    Name: "PI-18A",
+    ID: 292
+}, {
+    Name: "PI-19A",
+    ID: 293
+}, {
+    Name: "PI-20A",
+    ID: 294
+}];
+
+// programList and invStructureProgramMap values come from an inspection of the the dropdowns
 let programList = [79, 81, 83, 119, 130, 132, 239, 242, 243, 245, 246, 247, 268, 280, 294];
+
+let invStructureProgramMap = new Map();
+invStructureProgramMap.set(INTEGER-VALUE-OF-PROGRAM, 'PROGRAM-NAME-HERE');
 
 let showProgressMessages = true;
 
 outputArray = [];
-outputArray[0] = "PI,Epic,Program,Team,Estimate (Team Weeks)"
+outputArray[0] = "PI,Investable Structure,Epic,Program,Team,Estimate (Team Weeks)"
 
 loopCount = 1;
 
-//function to generate download of results
+//Function to generate download of results
 //from: https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
 function downloadResults(downloadFileName, downloadContent) {
     var element = document.createElement('a');
@@ -32,60 +54,61 @@ function downloadResults(downloadFileName, downloadContent) {
     document.body.removeChild(element);
 }
 
+// Main program loop to retrieve and format data
 for (const [epicKey, epicValue] of Object.entries(epicList)) {
-
     if (showProgressMessages) {
         console.log("--- Working on epic: ", epicValue);
     }
 
-    for (const [piKey, piValue] of Object.entries(piList)) {
+    //From the Epic, get the primaryProgramID to determine the investable structure originating the work
+    let result = await fetch(`https://YOUR-SUBDOMAIN-HERE.jiraalign.com/rest/align/api/2/Epics/${epicValue}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer YOUR-TOKEN-HERE`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error(response.statusText);
+        })
 
+    invStructureOriginating = (invStructureProgramMap.has(result.primaryProgramId) ? invStructureProgramMap.get(result.primaryProgramId) : 0);
+
+    for (const [piKey, piValue] of Object.entries(piList)) {
         if (showProgressMessages) {
-            console.log("--- --- Working on pi: ", piValue);
+            console.log("--- --- Working on pi: ", piValue.ID);
         }
 
         for (const [programKey, programValue] of Object.entries(programList)) {
-
             if (showProgressMessages) {
                 console.log("--- --- --- Working on program: ", programValue);
             }
-
-            let reqURL = https: //<your instance>.jiraalign.com/privateapi/teamForecasts?itemType=2&itemId=;
-
-            reqURL += epicValue;
-            reqURL += "&releaseid=";
-            reqURL += piValue;
-            reqURL += "&programId=";
-            reqURL += programValue;
-
-            await $.ajax({
-                url: reqURL,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                },
-                type: "GET",
-
-                success: await
-                function(result) {
-                    results = result.results;
-
-                    for (x = 0; x < results.length; x++) {
-                        let currentResult = ""
-                        currentResult += piValue + ","
-                        currentResult += epicValue + ","
-                        currentResult += "\"" + results[x].programName + "\","
-                        currentResult += "\"" + results[x].teamName + "\","
-                        currentResult += results[x].estimate
-
-                        outputArray[loopCount] = currentResult;
-                        loopCount++
+            
+            //Get team level estimates for each program in scope
+            let reqURL = https://YOUR-SUBDOMAIN-HERE.jiraalign.com/privateapi/teamForecasts?itemType=2&itemId= + epicValue;
+            reqURL += "&releaseid=" + piValue.ID;
+            reqURL += "&programId=" + programValue;
+            
+            let result = await fetch(reqURL, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
                     }
-                },
- 
-               error: function(error) {
-                    console.log("ERROR: ", error);
-                }
-            })
+                })
+                .then(response => {
+                    if (response.ok) return response.json();
+                    throw new Error(response.statusText);
+                })
+            
+            resultArray = result.results;
+            
+            for (x = 0; x < resultArray.length; x++) {
+                let currentResult = ""
+                currentResult += piValue.Name + "," + invStructureOriginating + "," + epicValue + "," + "\"" + resultArray[x].programName + "\"," + "\"" + resultArray[x].teamName + "\"," + ((resultArray[x].estimate == null) ? 0 : resultArray[x].estimate)
+                outputArray[loopCount] = currentResult;
+                loopCount++
+            }
         }
     }
 }
